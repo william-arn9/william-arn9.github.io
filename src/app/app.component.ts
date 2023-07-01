e473334acffe047b40245a74ffa61360b3b391a1
William Arnold <williamarn99@gmail.com>
2023-07-01 12:37:25 -0700

    Initial home page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { animate, state, style, transition, trigger } from '@angular/animations';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,4 +8,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'portfolio-app';
+  name = 'William Arnold';
 }
---
+++ src/app/title-box/title-box.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-title-box',
+  templateUrl: './title-box.component.html',
+  styleUrls: ['./title-box.component.scss']
+})
+export class TitleBoxComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
