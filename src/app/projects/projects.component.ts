795616d629d68872d1ab1db3221011d1af944705
William Arnold <williamarn99@gmail.com>
2023-07-15 13:24:36 -0700

    Mobile styling

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-projects',
@@ -7,9 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProjectsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private renderer: Renderer2) { }
 
   ngOnInit(): void {
+  }
+
+  linkOut(url: string): void {
+    this.renderer.setAttribute(window.open(url, '_blank'), 'noopener', 'true');
   }
 
 }
